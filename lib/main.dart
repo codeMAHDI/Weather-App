7074e82b8995bcc7b73562afc62ddd87192c623e
Moshiur Rahman (Mahdi) <89650128+codeMAHDI@users.noreply.github.com>
2025-12-15 01:05:05 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 void main() {
-  runApp(const MyApp());
-}
+  runApp(const MyApp());}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
